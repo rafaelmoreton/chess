3335e1fdb1fe8dc459675a004f7eeb5518d5469d
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-03-05 17:05:11 -0300

    Feat: Print board to terminal window
    [Array structure favored in place of a hash to represent the board grid]
    Since we'll have to frequently iterate through the board's elements with
    methods that always return arrays we would have to always convert it back
    to a hash.

--- lib/board.rb
+++ lib/board.rb
@@ -6,21 +6,50 @@ class Board
   end
 
   def new_grid
-    grid = {}
-    row_letter = 'a'
+    grid = []
+    row_letter = 8
     8.times do
-      grid = grid.merge(new_row(row_letter))
-      row_letter = row_letter.succ
+      grid << new_row(row_letter)
+      row_letter = row_letter.pred
     end
     grid
   end
 
+  def show
+    checkered_board = @grid.map.with_index do |row, row_index|
+      if row_index.even?
+        row.map.with_index do |_square, sqr_index|
+          if sqr_index.even?
+            "\e[1m\u001b[90m\e[37m   \u001b[0m\e[0m"
+          else
+            "\e[1m\u001b[44m\e[37m   \u001b[0m\e[0m"
+          end
+        end
+      else
+        row.map.with_index do |_square, sqr_index|
+          if sqr_index.odd?
+            "\e[1m\u001b[90m\e[37m   \u001b[0m\e[0m"
+          else
+            "\e[1m\u001b[44m\e[37m   \u001b[0m\e[0m"
+          end
+        end
+      end
+    end
+    wrapped_board = checkered_board.map do |row|
+      row[-1] += "\n"
+      row
+    end
+    puts wrapped_board.join
+  end
+
+  private
+
   def new_row(letter)
-    row = {}
-    column = '1'
+    row = []
+    column = 'a'
     8.times do
-      position = (letter + column).to_sym
-      row[position] = nil
+      position = letter.to_s + column
+      row << position
       column = column.succ
     end
     row
