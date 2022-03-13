07c1bac8cd12b8e71a43be7bca3aab9fcabf5177
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-03-12 23:14:51 -0300

    Feat: Enforce tower movement rules
    Tests for tower have a dependency on Board. Since tower methods are only
    different implementations of #find_direction_coordinates their critical logic
    is already tested in isolation on piece_spec

--- lib/pieces/tower.rb
+++ lib/pieces/tower.rb
@@ -2,12 +2,26 @@
 
 require_relative 'piece'
 
+# Specify rules to find tower's valid movements
 class Tower < Piece
   def initialize(color)
     super
     @symbol = color == 'white' ? "\u265c" : "\u2656"
   end
 
-  def horizontal
+  def valid_moves(board)
+    horizontal(board) + vertical(board)
+  end
+
+  def horizontal(board)
+    left = find_direction_coordinates([-1, 0], board)
+    right = find_direction_coordinates([1, 0], board)
+    left + right
+  end
+
+  def vertical(board)
+    up = find_direction_coordinates([0, 1], board)
+    down = find_direction_coordinates([0, -1], board)
+    up + down
   end
 end
--- spec/tower_spec.rb
+++ spec/tower_spec.rb
@@ -1,44 +1,81 @@
 # frozen_string_literal: true
 
 require_relative '../lib/pieces/tower'
+require_relative '../lib/board'
 
 describe Tower do
-  subject(:w_tower) { described_class.new('white') }
-  sqr_symbols = %i[
-    a5 b5 c5 d5 e5
-    a4 b4 c4 d4 e4
-    a3 b3 c3 d3 e3
-    a2 b2 c2 d2 e2
-    a1 b1 c1 d1 e1
-  ]
-  sqr_symbols.each do |symbol|
-    let(symbol) { double('square', position: symbol.to_s, occupant: nil) }
-  end
-  let(:board) do
-    double(
-      'board',
-      grid:
-        [
-          a5, b5, c5, d5, e5,
-          a4, b4, c4, d4, e4,
-          a3, b3, c3, d3, e3,
-          a2, b2, c2, d2, e2,
-          a1, b1, c1, d1, e1
-        ]
-    )
-  end
+  subject(:tower) { described_class.new('white') }
+  let(:board) { Board.new }
 
   describe '#horizontal' do
     context 'when the tower is unobstructed' do
       context 'when it is at the center of the board' do
         before do
-          allow(c3).to receive(:occupant).and_return(w_tower)
+          board.add_piece(tower, 'c3')
         end
-        xit "returns an array with all squares of it's row, except it's own" do
-          row_array = %w[a3 b3 d3 e3]
-          result = w_tower.horizontal
+        it "returns an array with all squares of it's row except it's own" do
+          row_array = %w[a3 b3 d3 e3 f3 g3 h3]
+
+          result = tower.horizontal(board)
 
           expect(result).to match_array(row_array)
+        end
+      end
+
+      context 'when it at the end of the board' do
+        before do
+          board.add_piece(tower, 'a1')
+        end
+        it "returns an array with all squares of it's row except it's own" do
+          row_array = %w[c1 b1 d1 e1 f1 g1 h1]
+
+          result = tower.horizontal(board)
+
+          expect(result).to match_array(row_array)
+        end
+      end
+    end
+
+    context 'when the tower is obstructed' do
+      before do
+        board.add_piece(tower, 'g2')
+      end
+      context "when it's a same color piece" do
+        let(:obstructing_white) { double('piece', color: 'white') }
+        it 'stops the line at the obstruction position not including it' do
+          column_array = %w[h2 d2 e2 f2]
+          board.add_piece(obstructing_white, 'c2')
+
+          result = tower.horizontal(board)
+
+          expect(result).to match_array(column_array)
+        end
+      end
+      context "when it's an opposite color piece" do
+        let(:obstructing_black) { double('piece', color: 'black') }
+        it 'stops the line at the obstruction position including it' do
+          column_array = %w[h2 d2 e2 f2 c2]
+          board.add_piece(obstructing_black, 'c2')
+
+          result = tower.horizontal(board)
+
+          expect(result).to match_array(column_array)
+        end
+      end
+    end
+  end
+
+  describe '#vertical' do
+    context 'when the tower is unobstructed' do
+      context 'when it is at the center of the board' do
+        before do
+          board.add_piece(tower, 'c3')
+        end
+        it "returns an array with all squares of it's column except it's own" do
+          column_array = %w[c4 c5 c6 c7 c8 c2 c1]
+          result = tower.vertical(board)
+
+          expect(result).to match_array(column_array)
         end
       end
     end
